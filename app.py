40e19d3f0930524f3253d1df8f44f9c5b64f0456
PapaTeacher <papateacher2021@gmail.com>
2026-05-04 23:04:32 +0100

    Fix: Lock versions and update import paths

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI 
 from langchain_community.vectorstores import Chroma
-from langchain_community.document_loaders import PyPDFLoader
+# from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain.chains import RetrievalQA
 
 # --- 1. PERSISTENT PATH CONFIGURATION ---
 # These folders are mapped to the Render Disk
