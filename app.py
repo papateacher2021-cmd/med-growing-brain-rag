48a0156b331dc8f4b7f4986fa4e65d2cf053701c
PapaTeacher <papateacher2021@gmail.com>
2026-05-04 22:47:20 +0100

    Fix: Correct import to langchain_text_splitters (plural)

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 
 # --- 1. PERSISTENT PATH CONFIGURATION ---
